85606eec33de5e6b92a658b8baf79ce83527a0c2
kidkoder432 <findprajju@gmail.com>
2024-10-18 20:28:03 -0700

    added complementary filter for altitude (UNTESTED) and temperature compensation

--- include/alt.h
+++ include/alt.h
@@ -1,8 +1,6 @@
 #include "Arduino_LPS22HB.h"
 #include <math.h>
 
-const double PRESSURE_REF_ZERO_AGL = 100.826;
-
 double calculateOffset(float pressureRef) {
     double avgPressure = 0;
 
@@ -14,7 +12,17 @@ double calculateOffset(float pressureRef) {
 }
 
 double getAltitude(float pressureRef, float offset) {
-    Serial.println(pressureRef);
-    Serial.println(BARO.readPressure());
-    return 44330 * (1 - pow((BARO.readPressure() - offset) / pressureRef, 1 / 5.255f));
+
+    float TEMP = BARO.readTemperature() + 273.15f;
+    return TEMP / 0.0065f * (1 - pow((BARO.readPressure() - offset) / pressureRef, 1 / 5.255f));
+}
+
+double getAltitude_compl(float pressureRef, float offset, SensorReadings readings, double alt, double vertvel, double dt) {
+
+    double alt_baro = getAltitude(pressureRef, offset);
+
+    double alt_acc = alt + vertvel * dt;
+
+    return alt_baro * 0.02 + alt_acc * 0.98;
+
 }
